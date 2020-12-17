ef10850c37bab65fc92e72d7e516f015455adbb3
jonybang <jonybange@gmail.com>
2020-12-17 17:25:22 +0200

    add http plugin

---
+++ src/services/http.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright ©️ 2019 GaltProject Society Construction and Terraforming Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka)
+ *
+ * Copyright ©️ 2019 Galt•Core Blockchain Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka) by
+ * [Basic Agreement](ipfs/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS)).
+ */
+
+import axios from 'axios';
+
+export default class Http {
+    static get(uri, params = null) {
+        return Http.request('get', uri, {params: params});
+    }
+    static post(uri, data) {
+        return Http.request('post', uri, data);
+    }
+    static put(uri, data) {
+        return Http.request('put', uri, data);
+    }
+    static remove(uri) {
+        return Http.request('remove', uri);
+    }
+
+    static request(method, uri, data = null){
+      return axios[method](uri, data).then((response) => {
+        return response;
+      });
+    }
+}
---
+++ src/services/http.plugin.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright ©️ 2019 GaltProject Society Construction and Terraforming Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka)
+ *
+ * Copyright ©️ 2019 Galt•Core Blockchain Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka) by
+ * [Basic Agreement](ipfs/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS)).
+ */
+
+import http from './http'
+export default {
+    http,
+    install (Vue, options) {
+        Vue.prototype.$http = http
+    }
+}
