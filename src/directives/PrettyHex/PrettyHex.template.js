2b33c79ce530b8b3a177d31ac427afec31702adc
MicrowaveDev <jonybange@gmail.com>
2023-06-21 08:09:56 +0800

    fix pretty-hex

--- src/directives/PrettyHex/PrettyHex.template.js
+++ src/directives/PrettyHex/PrettyHex.template.js
@@ -1,7 +1,6 @@
 module.exports = `
 <span class="pretty-hex-container">
     <md-icon v-if="highlightCurrentUser && isCurrentUserWallet">person</md-icon>
-
     <span v-if="to" @mouseover="mouseOver()" @mouseleave="mouseLeave()">
         <router-link :to="to">
             <span v-if="prefix">{{prefix}}&nbsp;</span><span v-if="localePrefix" v-locale="localePrefix"></span>{{showHex}}
@@ -12,10 +11,13 @@ module.exports = `
           <span v-if="prefix">{{prefix}}&nbsp;</span><span v-if="localePrefix" v-locale="localePrefix"></span>{{showHex}}
         </a>
     </span>
+    <span v-else-if="hexUrl" @mouseover="mouseOver()" @mouseleave="mouseLeave()" class="current-user-address">
+        <a :href="href" target="_blank">
+          <span v-if="prefix">{{prefix}}&nbsp;</span><span v-if="localePrefix" v-locale="localePrefix"></span>{{showHex}}
+        </a>
+    </span>
     <span v-else @mouseover="mouseOver()" @mouseleave="mouseLeave()" class="current-user-address"><span v-if="prefix">{{prefix}}&nbsp;</span><span v-if="localePrefix" v-locale="localePrefix"></span>{{showHex}}</span>
     <span class="separator">&nbsp;</span>
     <a href @click.prevent.stop="copyToClipboard()" class="icon-link"><md-icon class="small-icon fas fa-clipboard-check"></md-icon></a>
-    <span class="separator">&nbsp;</span>
-    <a v-if="hexUrl" :href="hexUrl" target="_blank" class="icon-link"><md-icon class="small-icon fas fa-globe-africa"></md-icon></a>
 </span>
 `;
